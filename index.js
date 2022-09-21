ddca29995d4461c9b458dfe43be641f1feee4ebb
Raman <rs.kala@hotmail.com>
2022-09-21 15:54:06 -0400

    Remove unneeded import

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const fs = require('fs');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
